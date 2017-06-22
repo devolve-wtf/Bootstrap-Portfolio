85ad82d0486890b8a8647caab3796cad9480900e
kenput3r <devolve.wtf@gmail.com>
2017-06-22 12:02:02 -0700

    Adjust openModel() on scroll margin change

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -52,7 +52,11 @@ function openModal() {
 	var y = 0;
 
 	window.onscroll = function(e) {
-		y = window.scrollY - 60 + 'px';
+		if(window.scrollY > 50) {
+			y = window.scrollY - 50 + 'px';
+		}else{
+			y = 0;
+		}
 	};
 
 	for(var i = 0; i < thumbnails.length; i++) {
